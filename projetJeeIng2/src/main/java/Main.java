cbd87135e6233c3d50f3201718d600e817cc20bf
captainwhynot <83647053+captainwhynot@users.noreply.github.com>
2023-11-07 14:10:02 +0100

    Delete projetJeeIng2 directory

--- projetJeeIng2/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import conn.HibernateUtil;
-import dao.UserDao;
-
-public class Main {
-	public static void main(String[] args) {
-		UserDao dao= new UserDao(HibernateUtil.getSessionFactory());
-		dao.getUser("tyaty","tyty");
-	}
-}
