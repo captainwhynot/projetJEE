4d113fa62a8dec583b7fd4474b4eda0f0f7c5029
captainwhynot <83647053+captainwhynot@users.noreply.github.com>
2023-11-08 22:05:14 +0100

    commit poubelle

--- projetJeeIng2/src/main/java/Main.java
+++ projetJeeIng2/src/main/java/Main.java
@@ -1,41 +1,30 @@
-import java.util.List;
 
-import org.hibernate.SessionFactory;
+
+import dao.CreditCardDao;
+import dao.CustomerDao;
+import entity.CreditCard;
+import entity.Customer;
 
 import conn.HibernateUtil;
-import dao.*;
-import entity.*;
-
 public class Main {
 	public static void main(String[] args) {
-		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-		AdministratorDao adminDao = new AdministratorDao(sessionFactory);
-		Administrator admin = new Administrator("mailAdmin", "password", "admin");
-		//adminDao.saveAdmin(admin);
+		//CustomerDao dao = new CustomerDao(HibernateUtil.getSessionFactory());
+		//Customer customer = new Customer("mail2", "password2", "username2");
+		//dao.saveCustomer(customer);
+		//Customer cust = dao.getCustomerById(1);
+		//List<Customer> liste = dao.getAllCustomer();
+		
+		
+		//System.out.println("testSET: "+dao.setFidelityPoint(cust, 1));
+		
+		//dao.getCustomerConnexion("username1", "password1");
+		CreditCardDao dao = new CreditCardDao(HibernateUtil.getSessionFactory());
+		//CreditCard cb = new CreditCard(1,123,123,new Date());
+		//cb.setCredit(10);
+		//dao.saveCreditCard(cb);
+		//System.out.println(dao.getSolde(123, 11));
+		System.out.println(dao.setCredit(123, -10));
 
-
-		ModeratorDao modoDao = new ModeratorDao(sessionFactory);
-		Moderator modo = new Moderator("mailModo", "password", "modo");
-		System.out.println(modoDao.addModerator(modo));
-		System.out.println(modoDao.modifyRight(modo, "addProduct", true));
-		//System.out.println(modoDao.deleteModerator(modo));
-		
-		
-		CustomerDao customerDao = new CustomerDao(sessionFactory);
-		Customer customer = new Customer("mailCustomer", "password", "customer");
-		customerDao.saveCustomer(customer);
-		Customer cust = customerDao.getCustomerById(1);
-		List<Customer> liste = customerDao.getAllCustomer();
-		
-		System.out.println(cust.getUsername());
-		System.out.println(liste.get(0).getUsername());
-		System.out.println(customerDao.setFidelityPoint(cust, 10));
-		System.out.println(customerDao.setFidelityPoint(cust, -5));
-		
-		ProductDao productDao = new ProductDao(sessionFactory);
-		Product product = new Product("poster", 15.99, 5, modo.getId());
-		System.out.println(productDao.addProduct(product));
-		System.out.println(productDao.modifyProduct(product, product.getName(), 50.15, product.getStock()));
 		
 		
 	}
