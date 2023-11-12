29cb00efe970a6902361c1ba739d4430457c548f
ChristopheCai <caichristo@cy-tech.fr>
2023-11-12 18:37:08 +0100

    Manage Moderator Rights

--- projetJeeIng2/src/main/java/servlet/ServletManageModerator.java
+++ projetJeeIng2/src/main/java/servlet/ServletManageModerator.java
@@ -1,11 +1,24 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import conn.HibernateUtil;
+import dao.ModeratorDao;
+import dao.UserDao;
+import entity.Moderator;
+import entity.User;
 
 /**
  * Servlet implementation class ServletManageModerator
@@ -27,6 +40,12 @@ public class ServletManageModerator extends HttpServlet {
    	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
    	 */
    	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		
+		ModeratorDao moderatorDao = new ModeratorDao(sessionFactory);
+		List<Moderator> moderatorList = moderatorDao.getModeratorList();
+		
+	    request.setAttribute("moderatorList", moderatorList);
    		this.getServletContext().getRequestDispatcher("/manageModerator.jsp").include(request, response);
    	}
 
@@ -35,6 +54,35 @@ public class ServletManageModerator extends HttpServlet {
    	 */
    	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
    		doGet(request, response);
+   		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		String[] addProductList = request.getParameterValues("addProductList");
+		String[] modifyProductList = request.getParameterValues("modifyProductList");
+		String[] deleteProductList = request.getParameterValues("deleteProductList");
+		String[] moderatorList = request.getParameterValues("moderatorList");
+		
+		if (moderatorList != null) {
+			Moderator moderator = null;
+    		for (String email: moderatorList) {
+    			ModeratorDao moderatorDao = new ModeratorDao(sessionFactory);
+    			moderator = moderatorDao.getModerator(email);
+    			if (addProductList != null && Arrays.asList(addProductList).contains(email)) {
+    				 moderatorDao.modifyRight(moderator, "addProduct", true);
+    			} else {
+   				 	moderatorDao.modifyRight(moderator, "addProduct", false);
+    			}
+    			if (modifyProductList != null && Arrays.asList(modifyProductList).contains(email)) {
+   				 	moderatorDao.modifyRight(moderator, "modifyProduct", true);
+    			} else {
+   				 	moderatorDao.modifyRight(moderator, "modifyProduct", false);
+    			}
+    			if (deleteProductList != null && Arrays.asList(deleteProductList).contains(email)) {
+   				 	moderatorDao.modifyRight(moderator, "deleteProduct", true);
+   				} else {
+    				moderatorDao.modifyRight(moderator, "deleteProduct", false);
+				}
+    		}
+		}
+		response.sendRedirect("./ManageModerator");
    	}
 
 }
--- projetJeeIng2/src/main/webapp/manageModerator.jsp
+++ projetJeeIng2/src/main/webapp/manageModerator.jsp
@@ -1,14 +1,52 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import="java.util.List" %>
+<%@ page import="entity.*" %>
+    
 <!DOCTYPE html>
 <html>
 <%@ include file="header.jsp" %>
 <body>
-  
+
     <div class="d-flex justify-content-center align-items-center page-container">
         <div class="interieurAddProduct">
             <div class="centered">
-                <h1>Formulaire pour gérer des modérators dans la DB</h1>
+                <h1>Moderator List</h1>
+                <form method="POST" action="ManageModerator">
+	                <table class="table">
+	                    <thead>
+	                        <tr>
+	                            <th>ID</th>
+	                            <th>Email</th>
+	                            <th>Username</th>
+	                            <th>AddProduct</th>
+	                            <th>ModifyProduct</th>
+	                            <th>DeleteProduct</th>
+	                        </tr>
+	                    </thead>
+	                    <tbody>
+	                        <% List<Moderator> moderatorList = (List<Moderator>) request.getAttribute("moderatorList");
+	                            for (Moderator moderator : moderatorList) {
+	                        %>
+	                            <tr>
+	                                <td><%= moderator.getId() %></td>
+	                                <td><%= moderator.getEmail() %>
+	                                <input type="hidden" name="moderatorList" value="<%= moderator.getEmail() %>"></td>
+	                                <td><%= moderator.getUsername() %></td>
+	                                <td><input type="checkbox" name="addProductList" value="<%= moderator.getEmail() %>"
+	                                <%= moderator.canAddProduct() ? "checked" : "" %>></td>
+	                                <td><input type="checkbox" name="modifyProductList" value="<%= moderator.getEmail() %>"
+	                                <%= moderator.canModifyProduct() ? "checked" : "" %>></td>
+	                                <td><input type="checkbox" name="deleteProductList" value="<%= moderator.getEmail() %>"
+	                                <%= moderator.canDeleteProduct() ? "checked" : "" %>></td>
+	                            </tr>
+	                        <%
+	                            }
+	                        %>
+	                    </tbody>
+	                </table>
+	            	<button type="submit">Confirm</button>
+	          </form>
         </div>
        
     </div>
